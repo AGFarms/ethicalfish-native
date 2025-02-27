585cf5dc34643dca19be90e4aa78c73c69492f78
eugene <gianyrox@gmail.com>
2025-02-26 23:42:16 -0500

    react native camera

--- app/(tabs)/fish.tsx
+++ app/(tabs)/fish.tsx
@@ -1,12 +1,13 @@
 import React, { useRef, useState } from 'react';
 import { View, Button, StyleSheet } from 'react-native';
-import { Camera, useCameraDevice } from 'react-native-vision-camera';
+import { Camera, useCameraDevice, useCameraPermission, useMicrophonePermission } from 'react-native-vision-camera';
 
 const FishRecognitionApp = () => {
   const device = useCameraDevice('back');
   const cameraRef = useRef<Camera>(null);
   const [isRecording, setIsRecording] = useState(false);
-
+  const { hasPermission: hasCameraPermission, requestPermission: requestCameraPermission } = useCameraPermission()
+  const { hasPermission: hasMicrophonePermission, requestPermission: requestMicrophonePermission } = useMicrophonePermission()
   if (!device) return <View style={styles.container} />;
 
   const startRecording = async () => {
